eaa92c26adf0ccf884df206b3bb52cb0d963a565
Mario Fernandp <mgamef@hotmail.com>
2018-08-31 21:00:03 -0500

    first commit

---
+++ src/main/java/com/tecsup/maven/lab02/lab_4C15_2018_1_MFPG_Project_3/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.maven.lab02.lab_4C15_2018_1_MFPG_Project_3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
